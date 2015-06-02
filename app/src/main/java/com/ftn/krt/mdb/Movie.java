182de885cfdccd51b09e1dc286a25fe908941158
Nikola Hardi <atlantic777@lugons.org>
2015-06-02 03:53:13 +0200

    Movie class field name renamed to title

--- app/src/main/java/com/ftn/krt/mdb/Movie.java
+++ app/src/main/java/com/ftn/krt/mdb/Movie.java
@@ -4,7 +4,7 @@ package com.ftn.krt.mdb;
  * Created by nikola on 6/2/15.
  */
 public class Movie {
-    public String name;
+    public String title;
     public String editor;
     public int year;
     public long hash;
