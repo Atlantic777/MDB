7695518bec5491692aecd5356b65e0b92d71c9f8
Nikola Hardi <atlantic777@lugons.org>
2015-06-02 03:05:52 +0200

    Add button - implemented on click listener

--- app/src/main/java/com/ftn/krt/mdb/MainActivity.java
+++ app/src/main/java/com/ftn/krt/mdb/MainActivity.java
@@ -1,15 +1,21 @@
 package com.ftn.krt.mdb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button)findViewById(R.id.addButton);
+        b.setOnClickListener(this);
     }
 
     @Override
@@ -32,5 +38,10 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, MovieEdit.class));
     }
 }
