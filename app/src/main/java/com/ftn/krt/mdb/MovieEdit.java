8da3823eb1929f0ff428df17156197b9f9209a48
Nikola Hardi <atlantic777@lugons.org>
2015-06-02 03:29:50 +0200

    MovieEdit is no longer ActionBarActivity, it's just Activity

--- app/src/main/java/com/ftn/krt/mdb/MovieEdit.java
+++ app/src/main/java/com/ftn/krt/mdb/MovieEdit.java
@@ -1,12 +1,13 @@
 package com.ftn.krt.mdb;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MovieEdit extends ActionBarActivity {
+public class MovieEdit extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
