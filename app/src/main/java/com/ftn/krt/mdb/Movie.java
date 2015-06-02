b21746a56b82e48889e8bb33e5510971ae533773
Nikola Hardi <atlantic777@lugons.org>
2015-06-02 02:28:43 +0200

    Movie class implemented

--- app/src/main/java/com/ftn/krt/mdb/Movie.java
+++ app/src/main/java/com/ftn/krt/mdb/Movie.java
@@ -4,4 +4,8 @@ package com.ftn.krt.mdb;
  * Created by nikola on 6/2/15.
  */
 public class Movie {
+    public String name;
+    public String editor;
+    public int year;
+    public long hash;
 }
